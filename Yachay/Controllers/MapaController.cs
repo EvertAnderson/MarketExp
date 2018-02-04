a93632afdb8be25d061891b725fa750c41228acc
EvertAnderson <evert.anderson.hl@gmail.com>
2018-02-03 22:11:55 -0500

    facebook api graph

--- Yachay/Controllers/MapaController.cs
+++ Yachay/Controllers/MapaController.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Facebook;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using Yachay.DAL;
 using Yachay.Entities;
+using System.Web.Helpers;
 
 namespace Yachay.Controllers
 {
@@ -52,6 +54,25 @@ namespace Yachay.Controllers
             x.Nombre.ToLower() ?? "").Contains(texto) || 
             x.PalabrasClave.Any(y => (y.ToLower() ?? "").Contains(texto)) ||
             x.Productos.Any(z => (z.ToLower() ?? "").Contains(texto))).ToList();*/
+            return Json(new { listaNodos }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult FbGraphRequest(double lat, double lng, string texto)
+        {
+            var client = new FacebookClient("EAACEdEose0cBAARm9gO7e96nVXUAY4nezDtR01yqO5Ekf3FKmp5VCysHbh62G4ZBV6DReUgamGIFjVpsE33gVFcmTBpmAquoZA3XzAVgLcw9dukSUMwshAyCZAy5wpu84AhLR9siD6cXmnqukRMhQ4oZBMXmmFymz9lfjIdZByaqP9ZCJsZCoTVhTJ9pHAZC9uEZD");
+            dynamic places = client.Get("search?type=place&q=" + texto +"&center=" + lat + "," + lng + "&distance=1500&fields=id,name,location") as IDictionary<string, object>;
+            //var ubicaciones = new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(me);
+
+            List<NodoDTO> listaNodos = new List<NodoDTO>();
+            foreach (var item in places["data"])
+            {
+                NodoDTO obj = new NodoDTO();
+                obj.Nombre = item["name"];
+                obj.Direccion_Latitud = Convert.ToString((item["location"])["latitude"]);
+                obj.Direccion_Longitud = Convert.ToString((item["location"])["longitude"]);
+                listaNodos.Add(obj);
+            }
             return Json(new { listaNodos }, JsonRequestBehavior.AllowGet);
         }
     }
